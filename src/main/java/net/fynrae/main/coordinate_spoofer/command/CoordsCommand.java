8eecfa994c58b511fdb70c05be5e5badd23d1d8a
Fynrae Lovosta <lephuchung9267@gmail.com>
2025-01-22 05:09:32 +0700

    Added /cds spoof random + fixed bugs

--- src/main/java/net/fynrae/main/coordinate_spoofer/command/CoordsCommand.java
+++ src/main/java/net/fynrae/main/coordinate_spoofer/command/CoordsCommand.java
@@ -3,6 +3,7 @@ package net.fynrae.main.coordinate_spoofer.command;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.DoubleArgumentType;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
@@ -11,10 +12,12 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
+import java.util.Random;
 import java.util.Stack;
 
 public class CoordsCommand {
     private static boolean spoofingEnabled = true;
+    private static boolean updateWhenPlayerMove = false;
     private static Double staticX = null;
     private static Double staticY = null;
     private static Double staticZ = null;
@@ -22,6 +25,12 @@ public class CoordsCommand {
     private static Double traditionalValueX = null;
     private static Double traditionalValueY = null;
     private static Double traditionalValueZ = null;
+    private static Double randomFromValueX = null;
+    private static Double randomToValueX = null;
+    private static Double randomFromValueY = null;
+    private static Double randomToValueY = null;
+    private static Double randomFromValueZ = null;
+    private static Double randomToValueZ = null;
 
     private static Stack<Double> historyStaticX = new Stack<>();
     private static Stack<Double> historyStaticY = new Stack<>();
@@ -30,6 +39,12 @@ public class CoordsCommand {
     private static Stack<Double> historyTraditionalValueX = new Stack<>();
     private static Stack<Double> historyTraditionalValueY = new Stack<>();
     private static Stack<Double> historyTraditionalValueZ = new Stack<>();
+    private static Stack<Double> historyRandomFromValueX = new Stack<>();
+    private static Stack<Double> historyRandomToValueX = new Stack<>();
+    private static Stack<Double> historyRandomFromValueY = new Stack<>();
+    private static Stack<Double> historyRandomToValueY = new Stack<>();
+    private static Stack<Double> historyRandomFromValueZ = new Stack<>();
+    private static Stack<Double> historyRandomToValueZ = new Stack<>();
 
     private static Stack<Double> redoStaticX = new Stack<>();
     private static Stack<Double> redoStaticY = new Stack<>();
@@ -38,6 +53,14 @@ public class CoordsCommand {
     private static Stack<Double> redoTraditionalValueX = new Stack<>();
     private static Stack<Double> redoTraditionalValueY = new Stack<>();
     private static Stack<Double> redoTraditionalValueZ = new Stack<>();
+    private static Stack<Double> redoRandomFromValueX = new Stack<>();
+    private static Stack<Double> redoRandomToValueX = new Stack<>();
+    private static Stack<Double> redoRandomFromValueY = new Stack<>();
+    private static Stack<Double> redoRandomToValueY = new Stack<>();
+    private static Stack<Double> redoRandomFromValueZ = new Stack<>();
+    private static Stack<Double> redoRandomToValueZ = new Stack<>();
+
+    private static final Random random = new Random();
 
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(ClientCommandManager.literal("cds")
@@ -76,7 +99,18 @@ public class CoordsCommand {
                                                                 context.getSource(),
                                                                 StringArgumentType.getString(context, "coordinate"),
                                                                 StringArgumentType.getString(context, "operator"),
-                                                                DoubleArgumentType.getDouble(context, "value")))))))));
+                                                                DoubleArgumentType.getDouble(context, "value")))))))
+                        .then(ClientCommandManager.literal("random")
+                                .then(ClientCommandManager.argument("updateWhenPlayerMove", BoolArgumentType.bool())
+                                        .then(ClientCommandManager.argument("coordinate", StringArgumentType.word())
+                                                .then(ClientCommandManager.argument("fromValue", DoubleArgumentType.doubleArg())
+                                                        .then(ClientCommandManager.argument("toValue", DoubleArgumentType.doubleArg())
+                                                                .executes(context -> spoofRandomCoordinate(
+                                                                        context.getSource(),
+                                                                        BoolArgumentType.getBool(context, "updateWhenPlayerMove"),
+                                                                        StringArgumentType.getString(context, "coordinate"),
+                                                                        DoubleArgumentType.getDouble(context, "fromValue"),
+                                                                        DoubleArgumentType.getDouble(context, "toValue"))))))))));
     }
 
     private static int executeCoords(FabricClientCommandSource source) {
@@ -108,13 +142,47 @@ public class CoordsCommand {
         traditionalValueX = null;
         traditionalValueY = null;
         traditionalValueZ = null;
-        Text message = Text.literal("Coordinate spoofing values have been reset to true values!").formatted(Formatting.GREEN);
+        randomFromValueX = null;
+        randomToValueX = null;
+        randomFromValueY = null;
+        randomToValueY = null;
+        randomFromValueZ = null;
+        randomToValueZ = null;
+        updateWhenPlayerMove = false;
+        // Clear undo and redo stacks
+        historyStaticX.clear();
+        historyStaticY.clear();
+        historyStaticZ.clear();
+        historyTraditionalOperator.clear();
+        historyTraditionalValueX.clear();
+        historyTraditionalValueY.clear();
+        historyTraditionalValueZ.clear();
+        historyRandomFromValueX.clear();
+        historyRandomToValueX.clear();
+        historyRandomFromValueY.clear();
+        historyRandomToValueY.clear();
+        historyRandomFromValueZ.clear();
+        historyRandomToValueZ.clear();
+        redoStaticX.clear();
+        redoStaticY.clear();
+        redoStaticZ.clear();
+        redoTraditionalOperator.clear();
+        redoTraditionalValueX.clear();
+        redoTraditionalValueY.clear();
+        redoTraditionalValueZ.clear();
+        redoRandomFromValueX.clear();
+        redoRandomToValueX.clear();
+        redoRandomFromValueY.clear();
+        redoRandomToValueY.clear();
+        redoRandomFromValueZ.clear();
+        redoRandomToValueZ.clear();
+        Text message = Text.literal("Coordinate spoofing values and history have been reset to true values!").formatted(Formatting.GREEN);
         MinecraftClient.getInstance().player.sendMessage(message, false);
         return Command.SINGLE_SUCCESS;
     }
 
     private static int undoSpoofing(FabricClientCommandSource source) {
-        if (historyStaticX.isEmpty() && historyTraditionalOperator.isEmpty()) {
+        if (historyStaticX.isEmpty() && historyTraditionalOperator.isEmpty() && historyRandomFromValueX.isEmpty()) {
             Text message = Text.literal("No undo actions available!").formatted(Formatting.RED);
             MinecraftClient.getInstance().player.sendMessage(message, false);
             return Command.SINGLE_SUCCESS;
@@ -147,13 +215,37 @@ public class CoordsCommand {
             redoTraditionalValueZ.push(traditionalValueZ);
             traditionalValueZ = historyTraditionalValueZ.pop();
         }
-        Text message = Text.literal("Coordinate spoofing values have been reverted to the previous spoofed values!").formatted(Formatting.GREEN);
+        if (!historyRandomFromValueX.isEmpty()) {
+            redoRandomFromValueX.push(randomFromValueX);
+            randomFromValueX = historyRandomFromValueX.pop();
+        }
+        if (!historyRandomToValueX.isEmpty()) {
+            redoRandomToValueX.push(randomToValueX);
+            randomToValueX = historyRandomToValueX.pop();
+        }
+        if (!historyRandomFromValueY.isEmpty()) {
+            redoRandomFromValueY.push(randomFromValueY);
+            randomFromValueY = historyRandomFromValueY.pop();
+        }
+        if (!historyRandomToValueY.isEmpty()) {
+            redoRandomToValueY.push(randomToValueY);
+            randomToValueY = historyRandomToValueY.pop();
+        }
+        if (!historyRandomFromValueZ.isEmpty()) {
+            redoRandomFromValueZ.push(randomFromValueZ);
+            randomFromValueZ = historyRandomFromValueZ.pop();
+        }
+        if (!historyRandomToValueZ.isEmpty()) {
+            redoRandomToValueZ.push(randomToValueZ);
+            randomToValueZ = historyRandomToValueZ.pop();
+        }
+        Text message = Text.literal("Redo the last undone spoofing values!").formatted(Formatting.GREEN);
         MinecraftClient.getInstance().player.sendMessage(message, false);
         return Command.SINGLE_SUCCESS;
     }
 
     private static int redoSpoofing(FabricClientCommandSource source) {
-        if (redoStaticX.isEmpty() && redoTraditionalOperator.isEmpty()) {
+        if (redoStaticX.isEmpty() && redoTraditionalOperator.isEmpty() && redoRandomFromValueX.isEmpty()) {
             Text message = Text.literal("No redo actions available!").formatted(Formatting.RED);
             MinecraftClient.getInstance().player.sendMessage(message, false);
             return Command.SINGLE_SUCCESS;
@@ -186,6 +278,30 @@ public class CoordsCommand {
             historyTraditionalValueZ.push(traditionalValueZ);
             traditionalValueZ = redoTraditionalValueZ.pop();
         }
+        if (!redoRandomFromValueX.isEmpty()) {
+            historyRandomFromValueX.push(randomFromValueX);
+            randomFromValueX = redoRandomFromValueX.pop();
+        }
+        if (!redoRandomToValueX.isEmpty()) {
+            historyRandomToValueX.push(randomToValueX);
+            randomToValueX = redoRandomToValueX.pop();
+        }
+        if (!redoRandomFromValueY.isEmpty()) {
+            historyRandomFromValueY.push(randomFromValueY);
+            randomFromValueY = redoRandomFromValueY.pop();
+        }
+        if (!redoRandomToValueY.isEmpty()) {
+            historyRandomToValueY.push(randomToValueY);
+            randomToValueY = redoRandomToValueY.pop();
+        }
+        if (!redoRandomFromValueZ.isEmpty()) {
+            historyRandomFromValueZ.push(randomFromValueZ);
+            randomFromValueZ = redoRandomFromValueZ.pop();
+        }
+        if (!redoRandomToValueZ.isEmpty()) {
+            historyRandomToValueZ.push(randomToValueZ);
+            randomToValueZ = redoRandomToValueZ.pop();
+        }
         Text message = Text.literal("Redo the last undone spoofing values!").formatted(Formatting.GREEN);
         MinecraftClient.getInstance().player.sendMessage(message, false);
         return Command.SINGLE_SUCCESS;
@@ -199,6 +315,12 @@ public class CoordsCommand {
         historyTraditionalValueX.push(traditionalValueX);
         historyTraditionalValueY.push(traditionalValueY);
         historyTraditionalValueZ.push(traditionalValueZ);
+        historyRandomFromValueX.push(randomFromValueX);
+        historyRandomToValueX.push(randomToValueX);
+        historyRandomFromValueY.push(randomFromValueY);
+        historyRandomToValueY.push(randomToValueY);
+        historyRandomFromValueZ.push(randomFromValueZ);
+        historyRandomToValueZ.push(randomToValueZ);
     }
 
     private static int spoofStaticCoordinate(FabricClientCommandSource source, String coordinate, double value) {
@@ -210,6 +332,12 @@ public class CoordsCommand {
         redoTraditionalValueX.clear();
         redoTraditionalValueY.clear();
         redoTraditionalValueZ.clear();
+        redoRandomFromValueX.clear();
+        redoRandomToValueX.clear();
+        redoRandomFromValueY.clear();
+        redoRandomToValueY.clear();
+        redoRandomFromValueZ.clear();
+        redoRandomToValueZ.clear();
         switch (coordinate.toLowerCase()) {
             case "x":
                 staticX = value;
@@ -239,6 +367,12 @@ public class CoordsCommand {
         redoTraditionalValueX.clear();
         redoTraditionalValueY.clear();
         redoTraditionalValueZ.clear();
+        redoRandomFromValueX.clear();
+        redoRandomToValueX.clear();
+        redoRandomFromValueY.clear();
+        redoRandomToValueY.clear();
+        redoRandomFromValueZ.clear();
+        redoRandomToValueZ.clear();
         switch (coordinate.toLowerCase()) {
             case "x":
                 traditionalValueX = applyOperation(traditionalValueX != null ? traditionalValueX : 0, operator, value);
@@ -258,6 +392,52 @@ public class CoordsCommand {
         Text message = Text.literal("Traditional spoof for " + coordinate.toUpperCase() + " coordinate set to: " + coordinate + " " + operator + " " + value + "!").formatted(Formatting.GREEN);
         MinecraftClient.getInstance().player.sendMessage(message, false);
         return Command.SINGLE_SUCCESS;
+    }
+
+    private static int spoofRandomCoordinate(FabricClientCommandSource source, boolean update, String coordinate, double fromValue, double toValue) {
+        saveCurrentSpoofValues();
+        redoStaticX.clear();
+        redoStaticY.clear();
+        redoStaticZ.clear();
+        redoTraditionalOperator.clear();
+        redoTraditionalValueX.clear();
+        redoTraditionalValueY.clear();
+        redoTraditionalValueZ.clear();
+        redoRandomFromValueX.clear();
+        redoRandomToValueX.clear();
+        redoRandomFromValueY.clear();
+        redoRandomToValueY.clear();
+        redoRandomFromValueZ.clear();
+        redoRandomToValueZ.clear();
+
+        updateWhenPlayerMove = update;
+
+        switch (coordinate.toLowerCase()) {
+            case "x":
+                randomFromValueX = fromValue;
+                randomToValueX = toValue;
+                break;
+            case "y":
+                randomFromValueY = fromValue;
+                randomToValueY = toValue;
+                break;
+            case "z":
+                randomFromValueZ = fromValue;
+                randomToValueZ = toValue;
+                break;
+            default:
+                Text message = Text.literal("Invalid coordinate! Use x, y, or z.").formatted(Formatting.RED);
+                MinecraftClient.getInstance().player.sendMessage(message, false);
+                return Command.SINGLE_SUCCESS;
+        }
+
+        Text message = Text.literal("Random spoof for " + coordinate.toUpperCase() + " coordinate set to random value between " + fromValue + " and " + toValue + (update ? " (updates when player moves)" : "") + "!").formatted(Formatting.GREEN);
+        MinecraftClient.getInstance().player.sendMessage(message, false);
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static double getRandomValue(double fromValue, double toValue) {
+        return fromValue + (toValue - fromValue) * random.nextDouble();
     }
 
     private static double applyOperation(double original, String operator, double value) {
@@ -309,5 +489,33 @@ public class CoordsCommand {
 
     public static Double getTraditionalValueZ() {
         return traditionalValueZ;
+    }
+
+    public static Double getRandomFromValueX() {
+        return randomFromValueX;
+    }
+
+    public static Double getRandomToValueX() {
+        return randomToValueX;
+    }
+
+    public static Double getRandomFromValueY() {
+        return randomFromValueY;
+    }
+
+    public static Double getRandomToValueY() {
+        return randomToValueY;
+    }
+
+    public static Double getRandomFromValueZ() {
+        return randomFromValueZ;
+    }
+
+    public static Double getRandomToValueZ() {
+        return randomToValueZ;
+    }
+
+    public static boolean isUpdateWhenPlayerMove() {
+        return updateWhenPlayerMove;
     }
 }
--- src/main/java/net/fynrae/main/coordinate_spoofer/mixin/DebugScreenMixin.java
+++ src/main/java/net/fynrae/main/coordinate_spoofer/mixin/DebugScreenMixin.java
@@ -13,6 +13,13 @@ import java.util.List;
 
 @Mixin(DebugHud.class)
 public class DebugScreenMixin {
+    private double lastPlayerX = Double.NaN;
+    private double lastPlayerY = Double.NaN;
+    private double lastPlayerZ = Double.NaN;
+    private double spoofedX = Double.NaN;
+    private double spoofedY = Double.NaN;
+    private double spoofedZ = Double.NaN;
+
     @Inject(method = "getLeftText", at = @At("RETURN"), cancellable = true)
     private void onGetLeftText(CallbackInfoReturnable<List<String>> info) {
         List<String> leftText = info.getReturnValue();
@@ -23,23 +30,59 @@ public class DebugScreenMixin {
             double y = player.getY();
             double z = player.getZ();
 
+            boolean playerMoved = player.getX() != lastPlayerX || player.getY() != lastPlayerY || player.getZ() != lastPlayerZ;
+
             if (CoordsCommand.getStaticX() != null) {
                 x = CoordsCommand.getStaticX();
-            } else if (CoordsCommand.getTraditionalValueX() != null) {
+            } else if (CoordsCommand.getTraditionalValueX() != null && CoordsCommand.getTraditionalOperator() != null) {
                 x = applyOperation(x, CoordsCommand.getTraditionalOperator(), CoordsCommand.getTraditionalValueX());
+            } else if (CoordsCommand.getRandomFromValueX() != null && CoordsCommand.getRandomToValueX() != null) {
+                if (CoordsCommand.isUpdateWhenPlayerMove() && playerMoved) {
+                    x = CoordsCommand.getRandomFromValueX() + (CoordsCommand.getRandomToValueX() - CoordsCommand.getRandomFromValueX()) * Math.random();
+                } else if (Double.isNaN(spoofedX)) {
+                    x = CoordsCommand.getRandomFromValueX() + (CoordsCommand.getRandomToValueX() - CoordsCommand.getRandomFromValueX()) * Math.random();
+                } else {
+                    x = spoofedX;
+                }
             }
 
             if (CoordsCommand.getStaticY() != null) {
                 y = CoordsCommand.getStaticY();
-            } else if (CoordsCommand.getTraditionalValueY() != null) {
+            } else if (CoordsCommand.getTraditionalValueY() != null && CoordsCommand.getTraditionalOperator() != null) {
                 y = applyOperation(y, CoordsCommand.getTraditionalOperator(), CoordsCommand.getTraditionalValueY());
+            } else if (CoordsCommand.getRandomFromValueY() != null && CoordsCommand.getRandomToValueY() != null) {
+                if (CoordsCommand.isUpdateWhenPlayerMove() && playerMoved) {
+                    y = CoordsCommand.getRandomFromValueY() + (CoordsCommand.getRandomToValueY() - CoordsCommand.getRandomFromValueY()) * Math.random();
+                } else if (Double.isNaN(spoofedY)) {
+                    y = CoordsCommand.getRandomFromValueY() + (CoordsCommand.getRandomToValueY() - CoordsCommand.getRandomFromValueY()) * Math.random();
+                } else {
+                    y = spoofedY;
+                }
             }
 
             if (CoordsCommand.getStaticZ() != null) {
                 z = CoordsCommand.getStaticZ();
-            } else if (CoordsCommand.getTraditionalValueZ() != null) {
+            } else if (CoordsCommand.getTraditionalValueZ() != null && CoordsCommand.getTraditionalOperator() != null) {
                 z = applyOperation(z, CoordsCommand.getTraditionalOperator(), CoordsCommand.getTraditionalValueZ());
+            } else if (CoordsCommand.getRandomFromValueZ() != null && CoordsCommand.getRandomToValueZ() != null) {
+                if (CoordsCommand.isUpdateWhenPlayerMove() && playerMoved) {
+                    z = CoordsCommand.getRandomFromValueZ() + (CoordsCommand.getRandomToValueZ() - CoordsCommand.getRandomFromValueZ()) * Math.random();
+                } else if (Double.isNaN(spoofedZ)) {
+                    z = CoordsCommand.getRandomFromValueZ() + (CoordsCommand.getRandomToValueZ() - CoordsCommand.getRandomFromValueZ()) * Math.random();
+                } else {
+                    z = spoofedZ;
+                }
             }
+
+            if (playerMoved) {
+                spoofedX = x;
+                spoofedY = y;
+                spoofedZ = z;
+            }
+
+            lastPlayerX = player.getX();
+            lastPlayerY = player.getY();
+            lastPlayerZ = player.getZ();
 
             int blockX = (int) x;
             int blockY = (int) y;
@@ -63,14 +106,21 @@ public class DebugScreenMixin {
     }
 
     private double applyOperation(double original, String operator, double value) {
-        return switch (operator) {
-            case "plus" -> original + value;
-            case "minus" -> original - value;
-            case "multiply" -> original * value;
-            case "divide" -> original / value;
-            case "power" -> Math.pow(original, value);
-            case "modulus" -> original % value;
-            default -> original;
-        };
+        switch (operator) {
+            case "plus":
+                return original + value;
+            case "minus":
+                return original - value;
+            case "multiply":
+                return original * value;
+            case "divide":
+                return original / value;
+            case "power":
+                return Math.pow(original, value);
+            case "modulus":
+                return original % value;
+            default:
+                return original;
+        }
     }
 }
